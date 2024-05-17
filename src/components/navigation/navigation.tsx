76fc6a46ecb29c887fd4747db506b22cea665e33
AdamBalu <a.baluseskul@gmail.com>
2024-05-17 09:10:23 +0200

    feat: closing nav

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,23 +1,71 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 
 import { NavigationItems } from '@/data/navigation-list';
+import { scrollToSection } from '@/common/functions/scroll-to-section';
 
-import { NavButton } from './nav-button';
+export const Navigation = () => {
+	useEffect(() => {
+		const navbarCheckbox = document.getElementById(
+			'navbar-checkbox'
+		) as HTMLInputElement;
+		const navbar = document.querySelector('.menuToggle');
+		const navbarItems = document.querySelectorAll('.menuItem button');
 
-export const Navigation = async () => (
-	<nav className="menu--right" role="navigation">
-		<div className="menuToggle">
-			<input type="checkbox" />
-			<span />
-			<span />
-			<span />
-			<ul className="menuItem rounded-bl-2xl">
-				{NavigationItems.map((item, _) => (
-					<li key={item.label} className="my-6">
-						<NavButton item={item} />
-					</li>
-				))}
-			</ul>
-		</div>
-	</nav>
-);
+		const closeNavbar = () => {
+			if (navbarCheckbox.checked) {
+				navbarCheckbox.checked = false;
+			}
+		};
+
+		// Close navbar when clicking on a navbar item
+		navbarItems.forEach(item => {
+			item.addEventListener('click', () => {
+				closeNavbar();
+			});
+		});
+
+		// Close navbar when clicking outside of the navbar
+		document.addEventListener('click', event => {
+			if (
+				navbar &&
+				navbarCheckbox.checked &&
+				!navbar.contains(event.target as Node) &&
+				event.target !== navbarCheckbox
+			) {
+				closeNavbar();
+			}
+		});
+
+		// Cleanup event listeners on component unmount
+		return () => {
+			navbarItems.forEach(item => {
+				item.removeEventListener('click', closeNavbar);
+			});
+			document.removeEventListener('click', closeNavbar);
+		};
+	}, []);
+
+	return (
+		<nav className="menu--right" role="navigation">
+			<div className="menuToggle">
+				<input type="checkbox" id="navbar-checkbox" />
+				<span />
+				<span />
+				<span />
+				<ul className="menuItem rounded-bl-2xl">
+					{NavigationItems.map((item, _) => (
+						<li key={item.label} className="my-6">
+							<button
+								onClick={() => scrollToSection(item.href)}
+								className="uppercase"
+							>
+								{item.label}
+							</button>
+						</li>
+					))}
+				</ul>
+			</div>
+		</nav>
+	);
+};
